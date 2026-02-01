cbcd5ed9265b605d2a1bc33b37ba694ad876cbef
WryyyDoPauzao <optilinklinks@gmail.com>
2026-02-01 20:25:24 -0300

    Update alakbab.py

--- alakbab.py
+++ alakbab.py
@@ -17,7 +17,6 @@ class VulnScanner:
         self.session = requests.Session()
         self.vulnerabilities = []
         
-        # User agents variados
         self.user_agents = [
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
